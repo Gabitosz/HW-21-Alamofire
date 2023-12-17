5b76b4734f3407cdc5c2ccc02e76491558098e90
Gabitosz <gabriel.zdravkovici313@gmail.com>
2023-12-17 18:20:22 +0200

    add modal view

---
+++ HW-21-Alamofire/Sources/Views/ModalViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ModalViewController.swift
+//  HW-21-Alamofire
+//
+//  Created by Gabriel Zdravkovici on 17.12.2023.
+//
+
+import UIKit
+
+class ModalViewController: UIViewController {
+    
+    // MARK: Outlets
+    
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 26)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        label.lineBreakMode = .byWordWrapping
+    
+        return label
+    }()
+    
+    lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var eventsLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    lazy var comicsLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 14)
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+        setupHierarchy()
+        setupLayout()
+    }
+    
+    // MARK: Setup
+    
+    private func setupView() {
+         view.backgroundColor = .systemBackground
+    }
+    
+    private func setupHierarchy() {
+        let views = [titleLabel, descriptionLabel, eventsLabel, comicsLabel]
+        views.forEach { view.addSubview($0) }
+    }
+    
+    private func setupLayout() {
+        
+        NSLayoutConstraint.activate([
+            
+            titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            titleLabel.widthAnchor.constraint(equalToConstant: 350),
+            
+            descriptionLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 70),
+            descriptionLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            descriptionLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            eventsLabel.topAnchor.constraint(equalTo: descriptionLabel.bottomAnchor, constant: 70),
+            eventsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            eventsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            
+            comicsLabel.topAnchor.constraint(equalTo: eventsLabel.bottomAnchor, constant: 50),
+            comicsLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            comicsLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+           
+        ])
+    }
+    
+}
